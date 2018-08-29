940c97d1816647128938dd09fc0d750979bd86e0
machy44 <machy44@gmail.com>
2018-08-29 23:34:06 +0200

    default data is inserted in table

--- src/components/RankTable.tsx
+++ src/components/RankTable.tsx
@@ -1,50 +1,135 @@
 import * as React from 'react';
 import { Row, Table } from 'react-bootstrap';
+import { connect } from 'react-redux';
 
-const headValues = ['Position', 'Club Name', 'Played', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Points'];
+const statAttributes = {
+  'position': 0,
+  // tslint:disable-next-line:object-literal-sort-keys
+  'club name': '',
+  'played': 0,
+  'w': 0,
+  'd': 0,
+  'l': 0,
+  'gf': 0,
+  'ga': 0,
+  'gd': 0,
+  'points': 0,
+};
 
-interface ITableHeader {
-  headValues: string[];
-}
+// interface ITableHeader {
+//   tableHeaders: string[];
+// }
 
-const TableHeader: React.SFC<ITableHeader> = (props) => {
+const TableHeader: React.SFC<any> = ({tableHeaders}) => {
   return (
     <thead>
       <tr>
-        {props.headValues.map((element, index) => <th key={index}>{element}</th>)}      
+        {Object.keys(tableHeaders).map((element, index) => <th key={index}>{element}</th>)}      
       </tr>
     </thead>
   )
 }
 
-const TableBody: React.SFC<{}> = () => {
+const TableBody: React.SFC<any> = ({ tableData }) => {
   return (
     <tbody>
-      <tr>
-        <td>1</td>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-      </tr>
-      <tr>
-        <td>2</td>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-      </tr>
+      {
+        tableData.map((element: any, index: any) => {
+          const dataRow: any = Object.keys(element).map((key) => {
+            return <td key={key}>{element[key]}</td>;
+          })
+          return <tr key={index}>{dataRow}</tr>
+        }
+        )
+      }
     </tbody>
   )
 }
 
-const RankTable: React.SFC<{}> = () => {
+const RankTable: React.SFC<any> = ({clubs}) => {
   return (
     <Row className="table-wrapper">
       <Table responsive={true}>
-        <TableHeader headValues={headValues}/>
-        <TableBody/>
+        <TableHeader 
+          tableHeaders={statAttributes}
+        />
+        { clubs && clubs.length
+          ? <TableBody
+              tableData = {clubs}
+            /> 
+          : <TableBody
+              tableData = {[]}
+            /> 
+        }
       </Table>
     </Row>
   )
 }
 
-export default RankTable;
+const defineClubsAndAttrs = (data: any) => {
+  const clubs: any = []
+  data[0].matches.forEach((element: any) => {
+    for(const key in element) {
+      if (element.hasOwnProperty(key)) {
+        // all attributes where data gonna be kept
+        clubs.push({...statAttributes, 'club name': key});
+      }
+    }
+  });
+  return clubs;
+}
+
+// const defineClubStatProps = (clubs: any) => {
+//   return clubs.map(element => {
+//     return element { 'jure': 'boban'}
+//   })
+// }
+
+// export default RankTable;
+// selected selected = {value: "Round 6", label: "Round 6", id: 6}
+// const defineClubsStats = (data: any, selected: any) => {
+//   // tslint:disable-next-line:no-console
+//   // tslint:disable-next-line:prefer-const
+//   // let clubsStats: any=[];
+//   for(let i=0; i < selected.id; i++) {
+//     // tslint:disable-next-line:no-console
+//     console.log('matches', data[i].matches.forEach((element: any) => {
+//       // tslint:disable-next-line:no-console
+//       console.log('element in forEach', element);
+//       for (const key in element) {
+//         if (element.hasOwnProperty(key)) {
+//             // do stuff
+//           // tslint:disable-next-line:no-console
+//           console.log('element', element);
+//           // tslint:disable-next-line:no-console
+//           console.log('key', key);
+//           // tslint:disable-next-line:no-console
+//           console.log('element[key]', element[key]);
+//         }
+//     }
+     
+//     }));
+//     // tslint:disable-next-line:no-console
+//     // console.log('matches Object keys', Object.keys(data[i].matches));
+//   }
+//   // tslint:disable-next-line:no-console
+//   console.log('selected defineClubsStats', selected);
+//   return data;
+// }
+
+const mapStateToProps =(state: any) => {
+  // const { data, selected } = state;
+  const { data } = state;
+  if (!data.length){ return {} };
+  const clubs: any = defineClubsAndAttrs(data);
+  // const clubsWithStatProps: any = defineClubStatProps(clubs);
+  // tslint:disable-next-line:no-console
+  console.log('clubs', clubs);
+  // const clubsStats: any = defineClubsStats(data, selected);
+  return { 
+    // clubsStats
+    clubs
+  }
+}
+
+export default connect(mapStateToProps, null)(RankTable);
--- src/components/ResultsList.tsx
+++ src/components/ResultsList.tsx
@@ -11,11 +11,20 @@ const resultDisplay = (result: any) => {
     if (result.hasOwnProperty(key)) {
       // tslint:disable-next-line:no-console
       if (count === 1) {
-        prettyDisplay.push(<React.Fragment><span>{key}</span><span style={{paddingLeft: 10}}>{result[key]}</span>-</React.Fragment>);
+        prettyDisplay.push(
+          <span key={key}>
+            <span>{key}</span>
+            <span style={{ paddingLeft: 10 }}>{result[key]}</span>
+            &#45;
+          </span>);
         count += 1;
       }
       else {
-        prettyDisplay.push(<React.Fragment><span>{result[key]}</span><span style={{paddingLeft: 10}}>{key}</span></React.Fragment>);
+        prettyDisplay.push(
+          <span key={key}>
+            <span>{result[key]}</span>
+            <span style={{ paddingLeft: 10 }}>{key}</span>
+          </span>);
       }
     }
   }
@@ -24,35 +33,36 @@ const resultDisplay = (result: any) => {
 
 const ResultsList: React.SFC<any> = ({ roundResults }) => {
   // tslint:disable-next-line:no-console
-  console.log('roundResults', roundResults);
+  // console.log('roundResults', roundResults);
   return (
     <Row >
-      <ListGroup className="list-group-wrapper" style={{ textAlign: 'center'}}>
-      { roundResults
-        ? roundResults.matches.map((element: any, index: any) => {
-          // tslint:disable-next-line:no-console
-          console.log('ListGroupItem element', element);
+      <h3>Round results</h3>
+      <ListGroup className="list-group-wrapper" style={{ textAlign: 'center' }}>
+        {roundResults
+          ? roundResults.matches.map((element: any, index: any) => {
+            // tslint:disable-next-line:no-console
+            // console.log('ListGroupItem element', element);
             return <ListGroupItem key={index}>{resultDisplay(element)}</ListGroupItem>
           })
-        : null
-      }
+          : null
+        }
       </ListGroup>
     </Row>
   )
 };
 
 const defineResultsList = (data: any, selected: any) => {
-  const roundResults =  data.filter((rounds: any) => {
+  const roundResults = data.filter((rounds: any) => {
     return rounds.round === selected.id;
   })
 
   return roundResults[0];
 }
 
-const mapStateToProps =(state: any) => {
+const mapStateToProps = (state: any) => {
   const { data, selected } = state;
   const roundResults: any = defineResultsList(data, selected);
-  return { 
+  return {
     roundResults
   }
 }
